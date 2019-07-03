9d95e34bf40fb3c6ca39b572ca6c70a0586f0375
Chengsi <gaochengsi@gmail.com>
2019-07-03 12:44:29 -0700

    add cash, total sum, portfolio gain/loss

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import BuySellForm from './BuySellForm';
+import { genericTypeAnnotation } from '@babel/types';
 
 const Holdings = styled.table`
   text-align: center;
@@ -11,45 +12,74 @@ const tr = styled.tr`
     background-color: #f2f2f2;
 `;
 
+const sumRowStyle = {
+    backgroundColor: '#f5f5f5',
+};
+
 export default class Portfolio extends Component {
     render() {
-        let holdings = "";
-        
-        holdings = this.props.holdings.map(h => (
-            <tr id={h.symbol}>
-                <th>{h.symbol}</th>
-                <th>${h.curPrice}</th>
-                <th>{h.shares}</th>
-                <th>${(h.curPrice * h.shares).toFixed(2)}</th>
-                <th style={{color: (h.curPrice - h.buyPrice) * h.shares >= 0 ? "green" : "red"}}>
-                    ${((h.curPrice - h.buyPrice) * h.shares).toFixed(2)}
-                </th>
-                <th>
-                    <BuySellForm id={h.symbol}
-                                 buySell="sell"
-                                 symbol={h.symbol}
-                                 price={h.curPrice}
-                                 triggerSell={this.props.triggerSell}
-                                />
-                </th>
-            </tr>
+        let total = this.props.cashBalance;
+        let holdings = [];
+        for (let h of this.props.holdings) {
+            total += h.curPrice * h.shares;
+            holdings.push(
+                <tr id={h.symbol + h.shares}>
+                    <td>{h.symbol}</td>
+                    <td>${h.curPrice}</td>
+                    <td>{h.shares}</td>
+                    <td>${(h.curPrice * h.shares).toFixed(2)}</td>
+                    <td style={{ color: (h.curPrice - h.buyPrice) * h.shares >= 0 ? "green" : "red" }}>
+                        ${((h.curPrice - h.buyPrice) * h.shares).toFixed(2)}
+                    </td>
+                    <td>
+                        <BuySellForm id={h.symbol}
+                            buySell="sell"
+                            symbol={h.symbol}
+                            price={h.curPrice}
+                            triggerSell={this.props.triggerSell}
+                        />
+                    </td>
+                </tr>
             )
-        )
+        }
+        let GLcolor = total - 5000 >= 0 ? "green" : "red";
+        let gainLoss = (total - 5000).toFixed(2);
+        let totalDisplay = total.toFixed(2);
+
         return (
             <div className="Portfolio">
-                <i class="fas fa-folder-open"> Porfolio</i> <br/>
+                <i className="fas fa-folder-open"> Porfolio</i> <br />
                 <Holdings id="currentHoldings">
-                    <tr>
-                        <th>Symbol</th>
-                        <th>Price</th>
-                        <th>Shares</th>
-                        <th>Total</th>
-                        <th>Gain/Loss</th>
-                        <th>Shares to sell/Proceeds</th>
-                        <th> </th>
-                    </tr>
-                    {holdings}
-
+                    <tbody>
+                        <tr>
+                            <th>Symbol</th>
+                            <th>Price</th>
+                            <th>Shares</th>
+                            <th>Total</th>
+                            <th>Gain/Loss</th>
+                            <th>Shares to sell/Proceeds</th>
+                            <th> </th>
+                        </tr>
+                        {holdings}
+                        <tr>
+                            <td>Cash</td>
+                            <td></td>
+                            <td></td>
+                            <td>${this.props.cashBalance}</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <tr style={sumRowStyle}>
+                            <th>Sum</th>
+                            <td></td>
+                            <td></td>
+                            <td>${totalDisplay}</td>
+                            <td style={{color: GLcolor}}>${gainLoss}</td>
+                            <td>Shares to sell/Proceeds</td>
+                            <td> </td>
+                        </tr>
+                    </tbody>
                 </Holdings>
             </div>
         )
